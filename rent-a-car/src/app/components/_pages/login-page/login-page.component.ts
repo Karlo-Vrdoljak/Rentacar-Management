73ad6e2e92cb6a72090b6c5d0453b48754938517
Karlo Vrdoljak <1karlo.vrdoljak@gmail.com>
2021-08-20 23:24:57 +0200

    login tweak

--- rent-a-car/src/app/components/_pages/login-page/login-page.component.ts
+++ rent-a-car/src/app/components/_pages/login-page/login-page.component.ts
@@ -1,16 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import { BaseClass } from 'src/app/_abstract/base.class';
+import { EInteractionReducer } from 'src/app/_consts/consts';
+import { AuthService } from 'src/app/_services/auth.service';
+import { use } from 'typescript-mix';
+import { Config } from './../../../_services/config';
 
+export interface LoginPageComponent extends BaseClass {}
 @Component({
-  selector: 'app-login-page',
-  templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.scss']
+	selector: 'app-login-page',
+	templateUrl: './login-page.component.html',
+	styleUrls: ['./login-page.component.scss'],
 })
 export class LoginPageComponent implements OnInit {
+	@use(BaseClass) this;
 
-  
-  constructor() { }
+	constructor(public config: Config, public router: Router, public auth: AuthService) {
+		this.destroy = new Subject();
+	}
 
-  ngOnInit(): void {
-  }
-
+	ngOnInit(): void {
+		this.config.currentInteraction.pipe(takeUntil(this.destroy)).subscribe((event) => {
+			if (event.id == EInteractionReducer.loggedIn) {
+				this.router.navigate(['/profile', this.auth.user?.pkUser]);
+			}
+		});
+	}
 }
